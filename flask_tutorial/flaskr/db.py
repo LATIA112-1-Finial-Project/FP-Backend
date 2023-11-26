94060b66bdcd2ea3b24b4abe71469aa50edc0b12
KutsunaSubaRya <index20010928@gmail.com>
2023-11-26 14:05:56 +0800

    fix: delete unusage sql schema

--- flask_tutorial/flaskr/db.py
+++ flask_tutorial/flaskr/db.py
@@ -16,31 +16,19 @@ Base.query = db_session.query_property()
 
 def get_db():
     session = db_session()
-    # if 'db' not in g:
-    #     g.db = sqlite3.connect(
-    #         current_app.config['DATABASE'],
-    #         detect_types=sqlite3.PARSE_DECLTYPES
-    #     )
-    #     g.db.row_factory = sqlite3.Row
     return session
 
 
 def close_db(e=None):
-    # db = g.pop('db', None)
-    #
-    # if db is not None:
-    #     db.close()
     db_session.close()
 
 
 def init_db():
+    """Clear the existing data and create new tables."""
     import flaskr.models.user
     import flaskr.models.post
+    # Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-    # db = get_db()
-    #
-    # with current_app.open_resource('schema.sql') as f:
-    #     db.executescript(f.read().decode('utf8'))
 
 
 @click.command('init-db')
