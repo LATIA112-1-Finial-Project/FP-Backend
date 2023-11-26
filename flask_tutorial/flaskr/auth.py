ac7eac1a72bd0f7df498d3722d4e822ecd4b6d19
KutsunaSubaRya <index20010928@gmail.com>
2023-11-26 14:05:25 +0800

    feat: change response redirect to json response

--- flask_tutorial/flaskr/auth.py
+++ flask_tutorial/flaskr/auth.py
@@ -1,13 +1,15 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, make_response, jsonify
 )
 
 from flaskr.db import get_db
 from flask_argon2 import Argon2
 from flaskr.models.user import User
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -30,19 +32,17 @@ def register():
                 u = User(username=username, password=Argon2().generate_password_hash(password))
                 db.add(u)
                 db.commit()
-                # db.execute(
-                #     "INSERT INTO user (username, password) VALUES (?, ?)",
-                #     (username, Argon2().generate_password_hash(password)),
-                # )
-                # db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
+            # username is already registered
+            except IntegrityError as e:
+                error = 'User {} is already registered.'.format(username)
             else:
-                return redirect(url_for("auth.login"))
+                response = jsonify({'message': 'User created successfully.'})
+                response.status_code = 201
+                return response
 
         flash(error)
 
-    return render_template('auth/register.html')
+    return jsonify({'message': 'Register failed.'}), 401
 
 
 @bp.route('/login', methods=('GET', 'POST'))
@@ -53,7 +53,9 @@ def login():
         db = get_db()
         error = None
         stmt = select(User).where(User.username == username)
+        print(stmt)
         user = db.scalar(stmt)
+        print(user.username)
         if user is None:
             error = 'Incorrect username.'
         elif not Argon2().check_password_hash(user.password, password):
@@ -62,11 +64,21 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user.id
-            return redirect(url_for('hello'))
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token="Bearer " + access_token)
 
         flash(error)
 
-    return render_template('auth/login.html')
+    return jsonify({'message': 'Login failed.'}), 401
+
+
+@bp.route('/protected', methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
 
 @bp.before_app_request
 def load_logged_in_user():
@@ -78,22 +90,19 @@ def load_logged_in_user():
         db = get_db()
         stmt = select(User).where(User.id == user_id)
         g.user = db.scalar(stmt)
-        # g.user = get_db().execute(
-        #     'SELECT * FROM user WHERE id = ?', (user_id,)
-        # ).fetchone()
 
 
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('auth.login'))
+    return jsonify({'message': 'Logout successfully, GoodBye!'}), 200
 
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.login'))
+            return jsonify({'error': 'Authentication required'}), 401
 
         return view(**kwargs)
 
