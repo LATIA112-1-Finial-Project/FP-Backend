dc631bec41f0310f0f72445dfc5f0e9d6e08c7c9
KutsunaSubaRya <index20010928@gmail.com>
2023-12-11 15:32:05 +0800

    feat: add reset password api

--- flask_tutorial/flaskr/api/user_setting.py
+++ flask_tutorial/flaskr/api/user_setting.py
@@ -11,6 +11,7 @@ from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identi
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from flask_cors import CORS, cross_origin
+from flaskr.utils import send_email
 
 bp_uni = Blueprint('bp_uni', __name__, url_prefix='/api/v1/auth')
 
@@ -45,3 +46,52 @@ def user_info():
         'msg': 'error',
         'data': 'Bad request'
     }), 400)
+
+
+# reset password api, if reset password successfully, then send email to notify user
+@bp_uni.route('/reset_password', methods=['POST'])
+@jwt_required()
+def reset_password():
+    data = request.get_json()
+    email = get_jwt_identity()
+    old_password = data['old_password']
+    new_password = data['new_password']
+    db = get_db()
+    stmt = select(User).where(User.email == email)
+    user = db.scalar(stmt)
+    if user is None:
+        return make_response(jsonify({
+            'code': 400,
+            'msg': 'error',
+            'data': 'User not found'
+        }), 400)
+    else:
+        # reset password to new_password, new password is hashed by argon2
+        if not Argon2().check_password_hash(user.password, old_password):
+            return make_response(jsonify({
+                'code': 400,
+                'msg': 'error',
+                'data': 'Old password is wrong'
+            }), 400)
+        else:
+            try:
+                u_new_password = Argon2().generate_password_hash(new_password)
+                # change user password to new password
+                user.password = u_new_password
+                # commit to db
+                db.commit()
+                # send email to notify user
+                html = render_template("accounts/reset_password.html", username=user.username)
+                send_email(user.email, "密碼重設成功通知", html)
+            except IntegrityError as e:
+                return make_response(jsonify({
+                    'code': 400,
+                    'msg': 'error',
+                    'data': 'Reset password failed'
+                }), 400)
+            else:
+                return make_response(jsonify({
+                    'code': 200,
+                    'msg': 'success',
+                    'data': 'Reset password successfully, please check your email'
+                }), 200)
